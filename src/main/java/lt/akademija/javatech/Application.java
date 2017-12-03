0a0ad24685351775074f68ef282867362b3f84ee
Andrius <olstounas@gmail.com>
2017-12-03 17:56:59 +0200

    all methods

--- src/main/java/lt/akademija/javatech/Application.java
+++ src/main/java/lt/akademija/javatech/Application.java
@@ -28,7 +28,7 @@ public class Application {
 		return new WebMvcConfigurerAdapter() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
+				registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
 			}
 		};
 	}
